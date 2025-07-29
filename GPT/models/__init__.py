e91eb498790bf2a364d525eb8d155904f3ff62d0
Hehe Fan <crane.h.fan@gmail.com>
2025-07-29 17:40:11 +0800

    Create __init__.py

---
+++ GPT/models/__init__.py
@@ -0,0 +1,3 @@
+from .transformer import gpt_mini, gpt_tiny, gpt_small, gpt_base, gpt_large
+from .translution import lution_gpt_mini, lution_gpt_tiny, lution_gpt_small, lution_gpt_base, lution_gpt_large
+from .alpha_translution import alution_gpt_mini, alution_gpt_tiny, alution_gpt_small, alution_gpt_base, alution_gpt_large
