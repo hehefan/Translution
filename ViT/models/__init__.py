413df0c58956240aaabbc60d6dce5bc524d80b69
Hehe Fan <crane.h.fan@gmail.com>
2025-08-14 15:33:08 +0800

    Create __init__.py

---
+++ ViT/models/__init__.py
@@ -0,0 +1,3 @@
+from .transformer import former_vit_mini, former_vit_tiny, former_vit_small, former_vit_base, former_vit_large
+from .translution import lution_vit_mini, lution_vit_tiny, lution_vit_small, lution_vit_base, lution_vit_large
+from .alpha_translution import alution_vit_mini, alution_vit_tiny, alution_vit_small, alution_vit_base, alution_vit_large
