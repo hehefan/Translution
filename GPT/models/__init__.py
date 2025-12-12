1665d33653113d22415792344dfb554c5db518f6
Hehe Fan <crane.h.fan@gmail.com>
2025-12-12 17:33:44 +0800

    Add hybrid architecture

--- GPT/models/__init__.py
+++ GPT/models/__init__.py
@@ -1,3 +1,7 @@
-from .transformer import former_gpt_tiny, former_gpt_mini, former_gpt_small, former_gpt_base, former_gpt_large
-from .translution import lution_gpt_tiny, lution_gpt_mini, lution_gpt_small, lution_gpt_base, lution_gpt_large
-from .alpha_translution import alution_gpt_tiny, alution_gpt_mini, alution_gpt_small, alution_gpt_base, alution_gpt_large
+from .transformer import former_tiny, former_mini, former_small, former_base, former_large
+from .translution import kv_tiny, kv_mini, kv_small, kv_base, kv_large
+from .translution import k_tiny, k_mini, k_small, k_base, k_large
+from .translution import v_tiny, v_mini, v_small, v_base, v_large
+from .lor_translution import lor_kv_tiny, lor_kv_mini, lor_kv_small, lor_kv_base, lor_kv_large
+from .lor_translution import lor_k_tiny, lor_k_mini, lor_k_small, lor_k_base, lor_k_large
+from .lor_translution import lor_v_tiny, lor_v_mini, lor_v_small, lor_v_base, lor_v_large
